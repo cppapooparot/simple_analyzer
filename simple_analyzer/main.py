67cceaeba27f961b0ffea975c43f4516bd9e6902
Narek <nsahakyan42@gmail.com>
2025-11-08 21:53:59 +0400

    feat: add main loop

--- simple_analyzer/main.py
+++ simple_analyzer/main.py
@@ -1,3 +1,7 @@
+from analyzer import Analyzer
+import random
+import time
+
 def read_config():
 	config_values = {}
 	with open("../config/config.txt", "r") as file:
@@ -9,5 +13,24 @@ def read_config():
 config = read_config()
 interval = config.get("interval")
 sequence_length = config.get("sequence_length")
-print(f"interval = {interval}")
-print(f"sequence_length = {sequence_length}")
+
+analyzer = Analyzer()
+
+while True:
+	num = random.randint(1, 100)
+	analyzer.add_number(num)
+	
+	if len(analyzer.numbers) > sequence_length:
+			analyzer.numbers.pop(0)
+	print("Numbers:", analyzer.numbers)
+	print("Even count:", analyzer.even_count())
+	print("Odd count:", analyzer.odd_count())
+	print("Highest number:", analyzer.highest_number())
+	print("Increasing pairs:", analyzer.increasing_pairs())
+	print("-" * 40)
+
+	current_seconds = time.localtime().tm_sec
+	if len(analyzer.numbers) == sequence_length and current_seconds == 0:
+		print("Stopping the loop.")
+		break
+	time.sleep(interval)
